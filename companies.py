cd15ff574a1e523aa1594f2929b62c3f3f75d153
Eli Finer <eli.finer@gmail.com>
2015-06-29 15:56:44 +0300

    fixed many parsing errors

--- companies.py
+++ companies.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 import requests
@@ -28,8 +29,17 @@ def get_company_data(company_url):
     tables = soup.select('body > center > table')
     name = tables[2].select('tr')[1].td.text
     address1 = tables[2].select('tr')[2].td.text
-    address2 = tables[2].select('tr')[3].select('td')[1].text
-    ticker = tables[2].a.text
+    try:
+        address2 = tables[2].select('tr')[3].select('td')[1].text
+        ticker = tables[2].select('tr')[3].get_text()
+    except IndexError:
+        address2 = tables[2].select('tr')[4].select('td')[1].text
+        ticker = tables[2].select('tr')[4].get_text()
+    ticker_rex = re.search('Ticker: ([A-Z ]+)', ticker)
+    if ticker_rex:
+        ticker = ticker_rex.group(1)
+    else:
+        ticker = ''
     industry_rows = tables[6].select('table')[1].table.select('tr')
     sector = industry_rows[4].select('td')[2].text
     industry = industry_rows[5].select('td')[2].text
@@ -47,6 +57,8 @@ def get_company_data(company_url):
     )
 
 if __name__ == '__main__':
+    # data = get_company_data('http://www.crmz.com/Report/ReportPreview.aspx?BusinessId=419')
+    # print(data)
     try:
         for state_url in get_state_urls():
             for company_url in get_company_urls(state_url):
